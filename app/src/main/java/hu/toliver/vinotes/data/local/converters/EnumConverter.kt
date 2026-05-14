667e16c8b0b1411e56d76097d4e2f9b96132a4b4
T0liver <taschnero@edu.bme.hu>
2026-05-14 12:06:44 +0200

    Adding level converter to float

--- app/src/main/java/hu/toliver/vinotes/data/local/converters/EnumConverter.kt
+++ app/src/main/java/hu/toliver/vinotes/data/local/converters/EnumConverter.kt
@@ -1,6 +1,7 @@
 package hu.toliver.vinotes.data.local.converters
 
 import androidx.room.TypeConverter
+import hu.toliver.vinotes.domain.model.enums.Level
 
 object EnumConverter {
     @TypeConverter
@@ -12,4 +13,13 @@ object EnumConverter {
     @TypeConverter
     fun <T : Enum<T>> toString(enumValue: T): String =
         enumValue.name
+
+    @TypeConverter
+    fun Level.toFloat(): Float = when (this) {
+        Level.LOW -> 0.2f
+        Level.MILD -> 0.4f
+        Level.MEDIUM -> 0.5f
+        Level.SOLID -> 0.75f
+        Level.HIGH -> 1.0f
+    }
 }
