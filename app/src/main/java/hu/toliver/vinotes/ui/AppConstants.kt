d2fe5c2d01f221ee8dedf7d9a8a4ea5b0cfd6e9f
T0liver <taschnero@edu.bme.hu>
2026-05-16 14:44:47 +0200

    Creating constats that can be used

---
+++ app/src/main/java/hu/toliver/vinotes/ui/AppConstants.kt
@@ -0,0 +1,13 @@
+package hu.toliver.vinotes.ui
+
+object AppConstants {
+    const val DEFAULT_CATALOG_URL = "https://vinotes.toliver.hu/"
+    const val GITHUB_URL          = "https://github.com/T0liver/ViNotes"
+    const val ICONS8_URL          = "https://icons8.com"
+    const val APP_AUTHOR          = "T0liver"
+    const val APP_YEAR            = "2026"
+    const val ABOUT_TEXT          =
+        "Készült $APP_YEAR-ban $APP_AUTHOR által, a kulturált borfogyasztás " +
+        "terjesztése érdekében."
+}
+
