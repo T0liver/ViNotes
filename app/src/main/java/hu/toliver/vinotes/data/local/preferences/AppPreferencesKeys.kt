abdc5480b7e1856882c8c7c0f8f82ca8730a6480
T0liver <taschnero@edu.bme.hu>
2026-05-16 16:02:16 +0200

    Creating the local AppPreferences

---
+++ app/src/main/java/hu/toliver/vinotes/data/local/preferences/AppPreferencesKeys.kt
@@ -0,0 +1,9 @@
+package hu.toliver.vinotes.data.local.preferences
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object AppPreferencesKeys {
+    val CATALOG_URL = stringPreferencesKey("catalog_url")
+    val USERNAME    = stringPreferencesKey("username")
+}
+
