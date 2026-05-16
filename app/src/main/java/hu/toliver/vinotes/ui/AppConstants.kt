5372107c6a6f75bcfc8bb2daf298e4569eade1a6
T0liver <taschnero@edu.bme.hu>
2026-05-16 15:59:58 +0200

    Updating constants

--- app/src/main/java/hu/toliver/vinotes/ui/AppConstants.kt
+++ app/src/main/java/hu/toliver/vinotes/ui/AppConstants.kt
@@ -2,12 +2,11 @@ package hu.toliver.vinotes.ui
 
 object AppConstants {
     const val DEFAULT_CATALOG_URL = "https://vinotes.toliver.hu/"
-    const val GITHUB_URL          = "https://github.com/T0liver/ViNotes"
-    const val ICONS8_URL          = "https://icons8.com"
-    const val APP_AUTHOR          = "T0liver"
-    const val APP_YEAR            = "2026"
-    const val ABOUT_TEXT          =
-        "Készült $APP_YEAR-ban $APP_AUTHOR által, a kulturált borfogyasztás " +
-        "terjesztése érdekében."
+    const val GITHUB_URL = "https://github.com/T0liver/ViNotes"
+    const val ICONS8_URL = "https://icons8.com"
+    const val APP_AUTHOR = "T0liver"
+    const val APP_YEAR = "2026"
+    const val ABOUT_TEXT =
+        "Made in $APP_YEAR by $APP_AUTHOR with love," +
+                "supporting the cultured enjoyment of wine. "
 }
-
