a36a6e34de7c77a199bd5227548822b220c8e3da
T0liver <taschnero@edu.bme.hu>
2026-05-16 20:07:41 +0200

    Implement the CatalogSyncRepository

--- app/src/main/java/hu/toliver/vinotes/domain/repository/CatalogSyncRepository.kt
+++ app/src/main/java/hu/toliver/vinotes/domain/repository/CatalogSyncRepository.kt
@@ -2,6 +2,7 @@ package hu.toliver.vinotes.domain.repository
 
 import android.net.Uri
 import hu.toliver.vinotes.domain.model.Wine
+import hu.toliver.vinotes.domain.model.sync.CatalogDelta
 import hu.toliver.vinotes.domain.model.sync.CatalogManifest
 import hu.toliver.vinotes.domain.model.sync.DeltaInfo
 import hu.toliver.vinotes.domain.model.sync.SyncMetadata
@@ -11,7 +12,7 @@ interface CatalogSyncRepository {
     suspend fun fetchManifest(): Result<CatalogManifest>
     suspend fun updateSyncMetadata(metadata: SyncMetadata): Result<Unit>
     suspend fun downloadFull(): Result<List<Wine>>
-    suspend fun downloadDelta(deltaInfo: DeltaInfo): Result<List<Wine>>
+    suspend fun downloadDelta(deltaInfo: DeltaInfo): Result<CatalogDelta>
     suspend fun importFromLocalFile(uri: Uri): Result<List<Wine>>
     fun getSyncMetadata(): Flow<SyncMetadata?>
 }
