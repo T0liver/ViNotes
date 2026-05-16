a6ed24302df5326d3f409a8b1199434e4464e2fb
T0liver <taschnero@edu.bme.hu>
2026-05-16 16:02:58 +0200

    Creating preferences modeul

---
+++ app/src/main/java/hu/toliver/vinotes/data/di/PreferencesModule.kt
@@ -0,0 +1,23 @@
+package hu.toliver.vinotes.data.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import hu.toliver.vinotes.data.local.preferences.appDataStore
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PreferencesModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+        context.appDataStore
+}
+
