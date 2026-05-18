4b908aa8a0e0fe86088e7e46047a12e3fc6afef7
T0liver <taschnero@edu.bme.hu>
2026-05-18 10:31:26 +0200

    Enable edge to edge
    Like this was the flag that we were missing

--- app/src/main/java/hu/toliver/vinotes/MainActivity.kt
+++ app/src/main/java/hu/toliver/vinotes/MainActivity.kt
@@ -3,6 +3,7 @@ package hu.toliver.vinotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ import hu.toliver.vinotes.ui.theme.ViNotesTheme
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             MainActivityContent()
