c865cc796c99c180ee4efcb93b88cfdc13e74b3e
T0liver <123989030+T0liver@users.noreply.github.com>
2026-09-13 14:32:32 +0200

    Changing the version code also
    Like f... play store, the version name is not the same but ooookay I will change this also....

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 24
         //noinspection OldTargetApi ... like why. the andorid 17 (sdk level 37) is still not out, why do you want to target it? also, the compile sdk is 36, so you can't target 37. just stop.
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
